417cc8c50e021552b2a3eec63fc32683f96b17cf
mohitkannekanti <mohit.kannekanti@gmail.com>
2021-07-23 14:17:06 +0530

    intial commit

---
+++ src/Components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+
+function Search() {
+    return (
+        <div>
+            <Button variant="contained">Search</Button>
+        </div>
+    )
+}
+
+export default Search
